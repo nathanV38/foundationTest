f09d5e3784d99312eee1ff5182664347f61df91e
ilyes mathieu <ilyes.mathieu@gmail.com>
2013-06-17 15:47:21 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TstFound::Application.config.secret_token = 'f26de305c6bed5a78725fcc88f957b5f37d8b49619b72126e4d22610b1f9ec8302afb4530fd0f5e629e7d857ea99851806f8a23b3957528fd9acb5fec425110f'
